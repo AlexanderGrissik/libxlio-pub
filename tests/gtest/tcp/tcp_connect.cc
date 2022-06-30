6310a07e6d4eb4a6489c5e09320a54e3e09cb7a8
Alexander Grissik <agrissik@nvidia.com>
2022-06-30 13:37:19 +0300

    issue: 3100949 Cont. for blocking socket connect race
    In case of blocking connect failure, the timer for the socket was already been added.
    However, when blocking socket fails the dest-entry is destroyed.
    Since the timer was alrady registered it may try send a SYN rexmit,
    but will fail with seg-fault since the dest entry had been destroyed.
    As a fix, we set tcp-state to close.
    As part of the fix we also do clean handling of second connect() attempt on the same socket,
    which already failed a connect. In this case we return ECONNABORTED as this scenario is
    currently unsupported.
    From conect() manual https://man7.org/linux/man-pages/man2/connect.2.html:
    "If connect() fails, consider the state of the socket as unspecified. Portable applications
    should close the socket and create a new one for reconnecting."
    
    Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

--- tests/gtest/tcp/tcp_connect.cc
+++ tests/gtest/tcp/tcp_connect.cc
@@ -223,7 +223,7 @@ TEST_F(tcp_connect, ti_4_rto_racing)
  *    Multiple connect on the same socket
  * @details
  */
-TEST_F(tcp_connect, DISABLED_ti_5_multi_connect)
+TEST_F(tcp_connect, ti_5_multi_connect)
 {
     int fd = tcp_base::sock_create();
     ASSERT_LE(0, fd);
@@ -289,9 +289,14 @@ TEST_F(tcp_connect, DISABLED_ti_5_multi_connect)
         rc = -1;
         fd = tcp_base::sock_create();
         if (fd > 0) {
-            rc = connect(fd, reinterpret_cast<const sockaddr *>(&server_addr), sizeof(server_addr));
-            if (rc != 0) {
-                log_trace("Final connected errno: %d\n", errno);
+            rc = setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+            EXPECT_EQ(0, rc);
+            if (rc == 0) {
+                rc = connect(fd, reinterpret_cast<const sockaddr *>(&server_addr),
+                             sizeof(server_addr));
+                if (rc != 0) {
+                    log_trace("Final connected errno: %d\n", errno);
+                }
             }
             close(fd);
         }
