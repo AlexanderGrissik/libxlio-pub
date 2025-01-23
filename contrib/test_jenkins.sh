2998ef52ff8ca3e37816f6d5bff63eeee804bf44
Alexander Grissik <agrissik@nvidia.com>
2025-01-23 15:26:00 +0000

    issue: 4158083 Add build and runtime test for DPCP and DOCA
    Add compilation and basic functionality test for all four cases of DPCP and DOCA paths.
    
    Signed-off-by: Alexander Grissik <agrissik@nvidia.com>

--- contrib/test_jenkins.sh
+++ contrib/test_jenkins.sh
@@ -106,20 +106,6 @@ i=0
 if [ "$TARGET" == "all" -o "$TARGET" == "default" ]; then
     export jenkins_target="default"
     export prefix=${jenkins_test_custom_prefix}/${jenkins_target}
-    if "$COMPILE_DPCP"; then
-        do_check_dpcp opt_value
-        if [ ! -z "${opt_value}" ]; then
-            target_list[$i]="default: --enable-nginx --with-dpcp=${opt_value}"
-        else
-            echo "Requested dpcp support can not be executed"
-    		do_err "target: $TARGET"
-    		exit 1
-        fi
-    else
-        echo "dpcp support was not requested"
-		# We need to define a minimum list of targets to iterate over later
-		target_list[$i]="default: --enable-nginx"
-    fi
     if "$COMPILE_DOCA"; then
         if do_compile_doca doca_path; then
             target_list[$i]="${target_list} --with-doca=${doca_path}"
